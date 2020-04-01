c35a0c8c3dbb70ebdb978d1f3ef6c621bf84da9c
xavioo <rucahoxavi@gmail.com>
2020-04-01 11:39:12 +0200

    articles operation CRUD

--- website/articles/urls.py
+++ website/articles/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
 
     path('', views.ArticleListView.as_view(), name='home' ),
     path('article/<int:pk>', views.ArticleDetailView.as_view(), name='article_page' ),
+    path('article/new/', views.ArticleCreateView.as_view(), name='new_article'),
+     path('article/<int:pk>/edit', views.ArticleUpdateView.as_view(), name='article_edit'),
+     path('article/<int:pk>/delete', views.ArticleDeleteView.as_view(), name='article_delete'),
 ]
--- website/articles/views.py
+++ website/articles/views.py
@@ -1,4 +1,6 @@
 from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView,UpdateView, DeleteView
+from django.urls import reverse_lazy
 
 # Create your views here.
 
@@ -14,3 +16,18 @@ class ArticleDetailView(DetailView):
     template_name = 'detail.html'
     context_object_name = 'batman'
 
+class ArticleCreateView(CreateView):
+    model = Article
+    template_name = 'article_new.html'
+    fields = '__all__'
+
+class ArticleUpdateView(UpdateView):
+    model = Article
+    template_name = 'article_edit.html'
+    fields = ['title', 'text']
+
+class ArticleDeleteView(DeleteView):
+    model = Article
+    template_name = 'article_delete.html'
+    context_object_name = 'batman'
+    success_url = reverse_lazy('home')    
